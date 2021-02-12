e7a958a26cd9d72a071558e4a9dbfd71b82aa2f1
Jean <puchit01200@gmail.com>
2021-02-11 20:51:56 -0500

    Avance validar formularios

--- front/src/components/Register.js
+++ front/src/components/Register.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import api from '../services/api.js'
 import swal from '../services/swal.js'
+import SimpleReactValidator from 'simple-react-validator';
 
 export class Register extends Component {
 
     constructor(props) {
         super(props);
+ 
+        this.validator = new SimpleReactValidator({
+            autoForceUpdate: this,
+            className: 'alert alert-danger',
+            // messages: {
+            //     email: 'El formato de email no es válido',
+            //     required: 'El campo es requerido.'
+            // },
+            locale: 'es'
+        });
+
         this.state = {
             email: null,
             password: null,
@@ -16,11 +28,16 @@ export class Register extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        api.post('register', this.state).then(data => {
-            swal.alerta(data, 'success')
-        }, error => {
-            console.log('error', error)
-        });
+
+        if (this.validator.allValid()) {
+            // api.post('register', this.state).then(data => {
+            //     swal.alerta(data, 'success')
+            // }, error => {
+            //     console.log('error', error)
+            // });
+        } else {
+            this.validator.showMessages();
+        }
     }
 
     onChange = e => {
@@ -39,21 +56,24 @@ export class Register extends Component {
                             <div className="form-group row">
                                 <label className="col-sm-4 col-form-label">Email <span className="text-danger">*</span></label>
                                 <div className="col-sm-8">
-                                    <input type="email" name="email" className="form-control" placeholder="user@example.com" onChange={this.onChange} value={this.state.title} />
+                                    <input type="email" name="email" className="form-control" placeholder="user@example.com" onChange={this.onChange} value={this.state.title} onBlur={() => this.validator.showMessageFor('email')} />
+                                    {this.validator.message('email', this.state.email, 'required|email')}
                                 </div>
                             </div>
 
                             <div className="form-group row">
                                 <label className="col-sm-4 col-form-label">Contraseña <span className="text-danger">*</span></label>
                                 <div className="col-sm-8">
-                                    <input type="password" name="password" className="form-control" onChange={this.onChange} value={this.state.title} />
+                                    <input type="password" name="password" className="form-control" onChange={this.onChange} value={this.state.title} onBlur={() => this.validator.showMessageFor('contraseña')} />
+                                    {this.validator.message('contraseña', this.state.password, 'required')}
                                 </div>
                             </div>
 
                             <div className="form-group row">
                                 <label className="col-sm-4 col-form-label">Confirmar contraseña <span className="text-danger">*</span></label>
                                 <div className="col-sm-8">
-                                    <input type="password" name="password_confirmation" className="form-control" onChange={this.onChange} value={this.state.title} />
+                                    <input type="password" name="password_confirmation" className="form-control" onChange={this.onChange} value={this.state.title} onBlur={() => this.validator.showMessageFor('confirmar contraseña')} />
+                                    {this.validator.message('confirmar contraseña', this.state.password_confirmation, 'required')}
                                 </div>
                             </div>
 
